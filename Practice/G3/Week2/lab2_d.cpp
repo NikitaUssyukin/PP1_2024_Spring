d1d21d608baaed63d8136f84022274661d00d2d4
Nikita Ussyukin <n.ussyukin@kbtu.kz>
2024-01-26 18:19:22 +0600

    upd

--- Practice/G3/Week2/lab2_d.cpp
+++ Practice/G3/Week2/lab2_d.cpp
@@ -15,7 +15,23 @@ int main() {
 
     n *= 2; // same as n = n * 2;
 
-    cout << ceil(double(n) / double(k)) << endl;
+    cout << (n + n % k) / k << endl;
 
     return 0;
+
+    // 3 2 - 6 sides of steaks, 2 spots on the pan
+    // 3
+
+
+    // 13 7 - 26 sides of steaks, 7 spots on the pan;  
+    // 4
+    // 12 7
+    // 24 7
+    // 3
+    // 3
+
+    // 24 + 3 = 27
+
+    // 26 / 7 = 3
+    // 26 % 7 = 5
 }
