cd07f39a2072bebeddd8f2d3f87b0060190f525b
Nikita <nikita@Nikitas-MacBook-Pro.local>
2024-02-27 15:42:20 +0600

    Practice G1 27.02

---
+++ Practice/G1/Week7/lab4_m_1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[n][n];
+
+    int x = 0;
+    int y = n - 1;
+
+    int cnt = 1;
+
+    while(cnt < n * n)
+    {
+        for(int j = x; j <= y; j++) {
+            a[x][j] = cnt++;
+        }
+
+        for(int i = x + 1; i <= y; i++) {
+            a[i][y] = cnt++;
+        }
+
+        for(int j = y - 1; j >= x; j--) {
+            a[y][j] = cnt++;
+        }
+
+        for(int i = y - 1; i > x; i--) {
+            a[i][x] = cnt++;
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                cout << a[i][j] << " ";
+            }
+            cout << endl;
+        }
+        x++;
+        y--;
+    }
+
+    return 0;
+}
