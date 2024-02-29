ff004f0bc7b542a144ed31e87e93df4112113081
NikitaUssyukin <nikus0108@gmail.com>
2024-02-29 18:59:46 +0600

    Lecture on 29.02

---
+++ Lecture/Week7/lab4_m_1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[n][n];
+
+    int x = 0;
+    int y = n - 1;
+
+    int cnt = 1;
+
+    while(x <= y) {
+
+        for(int j = x; j <= y; j++) {
+            a[x][j] = cnt++;
+        }
+
+        for(int i = x + 1; i <= y; i++) {
+            a[i][y] = cnt++;
+        }
+
+        for(int j = y - 1; j >= x; j--) {
+            a[y][j] = cnt++;
+        }
+
+        for(int i = y - 1; i > x; i--) {
+            a[i][x] = cnt++;
+        }
+
+        x++;
+        y--;
+
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
